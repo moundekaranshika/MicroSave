f77e2495b69904e5f2286e137b9216491bc761a6
Anshika Moundekar <moundekaranshika2@gmail.com>
2026-05-21 17:08:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
-require('dotenv').config();   // MUST BE FIRST LINE
+require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
+const transactionRoutes = require('./routes/transactionRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-console.log("MONGO_URI:", process.env.MONGO_URI);  // DEBUG LINE
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error(err));
 
-app.listen(5001, () => console.log("Server running"));
+app.use('/api', transactionRoutes);   // 👈 VERY IMPORTANT
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log("Server running");
+});
